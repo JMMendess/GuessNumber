d51bfbf2b944bdfd0d5ffc2199ff7ddc2e0690a1
Melquíades <melquiades.rodrigues@hotmail.com>
2022-03-02 14:05:44 -0300

    fix: guess the number correction

--- js/index.js
+++ js/index.js
@@ -7,11 +7,12 @@ let result = document.getElementById('result');
 
 const Guess = {
     max: 10,
-    attemptsNumber: 0,
+    attemptsNumber: -1,
     numberDrawn: function randomValue() {
-        return Math.round(math.random()* this.max);
+        return Math.round(Math.random()* this.max);
     }
 };
+
 let numberDrawn = Guess.numberDrawn();
 
 function updateAttempt(attempt, value){
@@ -20,6 +21,7 @@ function updateAttempt(attempt, value){
 
 function handlesubmit(e){ 
     e.preventDefault();
+    console.log("enviou form")
 
     let kick = document.getElementById('kick').value;
     
@@ -27,40 +29,49 @@ function handlesubmit(e){
         alert ('digite algum valor!')
         return;
     }
-};
- updateAttempt(attempt, ++Guess.attemptsNumber);
 
-
+    console.log(numberDrawn, kick)
 
     if(numberDrawn == kick) {
+        console.log("entrou ==")
         playAgain();
         status.innerHTML = 'Parabéns, você cagou!';
         result.style.transition = '0.4s';
         result.style.backgroundColor = '#37c978';
         result.style.color = '#fff';
         status.style.color = '#fff';
+        const btnJogar = document.getElementById('jogar')
+        btnJogar.setAttribute('disabled', 'disabled');
+        btnJogar.style.backgroundColor = '#ccc'
         clear();
         
     } else if(numberDrawn > kick){
+        console.log("entrou >")
         status.innerHTML = 'O número é Maior!';
         status.style.color = '#de4251';
         clear();
     
     }else if(numberDrawn < kick) {
+        console.log("entrou <")
         status.innerHTML = 'O número é menor';
         status.style.color = '#de4251';
         clear();
     }
+};
 
+updateAttempt(attempt, ++Guess.attemptsNumber);
 
 function playAgain() {
-    document.getElementById('btnRestart').style.display = 'flex';
+    const btnRestart = document.getElementById('btnRestart');
+    btnRestart.style.display = 'flex';
+    btnRestart.onclick = restart;
 };
 
 function restart(){
+    console.log('restart');
     document.location.reload(true);
 };
 
 function clear() {
-  ;  document.getElementById('kick').value = '';
+  document.getElementById('kick').value = '';
 };
